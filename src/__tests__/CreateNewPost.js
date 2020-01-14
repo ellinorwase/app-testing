a1fbe843dcea543fabb39d71c15d151a087524c8
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-14 11:24:50 +0100

    test.posts

---
+++ src/__tests__/CreateNewPost.js
@@ -0,0 +1,10 @@
+import CreateNewPost from '../components/CreateNewComment';
+
+// beforeEach(() => {
+//     jest.resetModules();
+//   });
+
+//   test('returns all posts', () => {
+//     const todoList = require('../src/Posts');
+//     expect(todoList.getAllTodos()).toHaveLength(3);
+//   });
---
+++ src/__tests__/Posts.js
@@ -0,0 +1,8 @@
+beforeEach(() => {
+//     jest.resetModules();
+//   });
+
+//   test('returns all posts', () => {
+//     const posts = require('../src/Posts');
+//     expect(posts.removePost()).toHaveLength(1);
+//   });
