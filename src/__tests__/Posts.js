f0f9b7ca00b5146ca4153ac1c1b06b075272702f
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-14 11:25:12 +0100

    test.posts

--- src/__tests__/Posts.js
+++
@@ -1,8 +0,0 @@
-beforeEach(() => {
-//     jest.resetModules();
-//   });
-
-//   test('returns all posts', () => {
-//     const posts = require('../src/Posts');
-//     expect(posts.removePost()).toHaveLength(1);
-//   });
