6a857603a90eb66a7d8407cfb1126c5b8002639a
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-15 09:29:18 +0100

    createPost-branch

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -1,10 +1,42 @@
-import CreateNewPost from '../components/CreateNewComment';
+import CreateNewPost from "../components/CreateNewPost";
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Index from "../api/index";
 
 // beforeEach(() => {
 //     jest.resetModules();
 //   });
 
-//   test('returns all posts', () => {
-//     const todoList = require('../src/Posts');
-//     expect(todoList.getAllTodos()).toHaveLength(3);
+// it('create a new post', () => {
+//     const title = "Titeln"
+//     const wrapper = mount(<CreateNewPost/>);
+//     expect(wrapper.state.title().title).toBe(title)
+//     expect(wrapper.find('#title')).toBe(title);
+//     // expect(wrapper.state().user).toBeFalsy();
+
+// });
+
+
+
+describe("New Post", () => {
+  //   it("create a new post", () => {
+  //     const wrapper = mount(<CreateNewPost />);
+  //     const title = "Titeln";
+  //     const content = "contents";
+  //     wrapper.find("#title").simulate("submit", {
+  //       target: { value: title }
+  //     });
+  //     wrapper.find("#content").simulate("submit", {
+  //       target: { value: content }
+  //     });
+  //     expect(wrapper.state().onSubmit).toEqual(title, content);
+  //   });
+//   test("title to be hej", () => {
+//     const beverage = { title: "hej" };
+//     const drink = jest.fn(beverage => beverage.title);
+
+//     drink(beverage);
+
+//     expect(drink).toHaveReturnedWith("hej");
 //   });
+});
