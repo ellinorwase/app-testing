201ed5d3b70036760b74ff46520fcf0dd1a8569c
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-15 09:55:01 +0100

    removed posts.js

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -1,10 +1,2 @@
 import CreateNewPost from '../components/CreateNewComment';
 
-// beforeEach(() => {
-//     jest.resetModules();
-//   });
-
-//   test('returns all posts', () => {
-//     const todoList = require('../src/Posts');
-//     expect(todoList.getAllTodos()).toHaveLength(3);
-//   });
