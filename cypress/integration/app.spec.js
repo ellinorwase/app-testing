5c88e2294b58baa17fddd69ce7836fcbc1cfeada
Frida Schoultz <fridaschoultz@hotmail.com>
2020-01-15 15:25:09 +0100

    adding cypress

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,3 +3,31 @@ describe('Login Test', function() {
       cy.visit('/');
     });
   });
+
+  describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(true)
+    })
+  })
+
+  describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(false)
+    })
+  })
+
+  describe('My First Test', function() {
+    it('Visits the Kitchen Sink', function() {
+      cy.visit('https://example.cypress.io')
+    })
+  })
+
+  describe('My First Test', function() {
+    it('finds the content "type"', function() {
+      cy.visit('https://example.cypress.io')
+  
+      cy.contains('hype')
+    })
+  })
+
+
