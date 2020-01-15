85ecdadf361b702c43e8777cd8f3f40ca2a64086
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-15 15:20:46 +0100

    find form

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -2,13 +2,8 @@ import CreateNewPost from '../components/CreateNewComment';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 
-describe('New post', () => {
-    it('should add a new post', () => {
-        const wrapper = mount(<CreateNewPost />);
-        (wrapper.find('#title')).simulate("change",{
-            target: {title: "hej"}
-        });
-        expect(wrapper.state().onSubmit).toBe("hej");
-      });
-})
-
+it("should contain form", () => {
+    const wrapper = mount(<CreateNewPost />);
+    expect(wrapper.find("form")).toHaveLength(1);
+  });
+  
